0669dd12bc8c36dc27ddb2d58cbb14c02e1cae01
Josh Nho <joshnho@gmail.com>
2019-08-06 02:49:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,13 +33,14 @@ end
 def hit?(card_total)
   prompt_user
   user_input = get_user_input
-  if user_input == "h"
-    card_total += deal_card
-  elsif user_input == "s"
-    card_total
-  else
-    invalid_command
-    get_user_input
+  until user_input == exit
+    if user_input == "h"
+      card_total += deal_card
+    elsif user_input == "s"
+      card_total
+    else
+      invalid_command
+    end
   end
 end
 
